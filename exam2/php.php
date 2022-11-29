72804b80a16a18feb4cf9aa6b29cc3e1656e31ea
Kybuk3 <93988006+Kybuk3@users.noreply.github.com>
2022-11-29 10:12:14 +0100

    Add files via upload

--- exam2/php.php
+++ exam2/php.php
@@ -1,25 +1,34 @@
 <?php 
-    $BASED="wedkowanie"
-    $USER="root"
-    $HOST="localhost"
-    $PASSWORD=""
+    $BASED="wedkowanie";
+    $USER="root";
+    $HOST="localhost";
+    $PASSWORD="";
 
     function Skrypt1(){
-        $conn=mysqli_connect($HOST,$USER,$PASSWORD,$BASED)
+        $BASED="wedkowanie";
+        $USER="root";
+        $HOST="localhost";
+        $PASSWORD="";
+        $conn=mysqli_connect($HOST,$USER,$PASSWORD,$BASED);
         $question="SELECT r.nazwa, akwen, wojewodstwo FROM Lowisko l Inner JOIN Ryby r On r.id=L.Ryby.id";
-        $result=$conn->query($question)
+        $result=$conn->query($question);
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-                echo "nazwa: " . $row[" r.nazwa"]. " plywa w rzece " . $row["akwen"]. " " . $row["wojewodstwo"]
-        $conn->close();}
+                echo "nazwa: " . $row[" r.nazwa"]. " plywa w rzece " . $row["akwen"]. " " . $row["wojewodstwo"];
+        $conn->close();
+    }
     }}
-    function Skrypt1(){
+    function Skrypt2(){
+        $BASED="wedkowanie";
+        $USER="root";
+        $HOST="localhost";
+        $PASSWORD="";
         $conn = new mysqli($HOST, $USER, $PASSWORD, $BASED);
         $sql = "SELECT * FROM Lowisko";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-                echo "" . $row["id"]. "". $row["Ryby_id"]. "" .$row["akwen"].""$row["akwen"]."".$row["wojewodstwo"]."".$row["rodzaj"]
+                echo "" . $row["id"]. "". $row["Ryby_id"]. "" .$row["akwen"]."".$row["akwen"]."".$row["wojewodstwo"]."".$row["rodzaj"];
             }
         $conn->close();
     }}
