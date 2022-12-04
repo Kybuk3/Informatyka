212aff64436010888060983b7352904f887c0e47
Kybuk3 <93988006+Kybuk3@users.noreply.github.com>
2022-12-04 13:15:39 +0100

    Update php.php

--- exam2/php.php
+++ exam2/php.php
@@ -4,12 +4,18 @@
     $password = "";
     $dbname = "wedkowanie";
     $conn = new mysqli($servername, $username, $password, $dbname);
+
     function Skrypt1(){
         global $servername, $username, $password, $dbname,$conn;
+        
         if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
         }
-        $sql = "SELECT nazwa, akwen, wojewodztwo FROM Lowisko l Inner JOIN Ryby r On r.id=l.Ryby_id";
+        
+        $sql = "SELECT nazwa, akwen, wojewodztwo
+        FROM Lowisko l
+        Inner JOIN Ryby r On r.id=l.Ryby_id";
+        
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
