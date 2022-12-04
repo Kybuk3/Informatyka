27ddfa197c3d437c841730915221b6121d81509f
Kybuk3 <93988006+Kybuk3@users.noreply.github.com>
2022-12-04 13:15:01 +0100

    Update php.php

--- exam2/php.php
+++ exam2/php.php
@@ -17,20 +17,7 @@
         }
         }}
     function Skrypt2(){
-        global $servername, $username, $password, $dbname,$conn;
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
         }
-        $sql = "SELECT id,nazwa,wystepowanie
-        from Ryby
-        Where styl_zycia = 1";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                echo "<div style='left:5.1%; position: relative; margin: 0px; top:18.1px;width:63.5%;display:flex;' onmouseover="."this.style.backgroundColor='#4FB3BF'"."; onmouseout="."this.style.backgroundColor='inherit'".";> <span style='border: 1px solid #4FB3DF;width:22.4%;text-align: center;'>" .$row["id"]." </span><span style='border: 1px solid #4FB3DF;width:31.6%;text-align: center;'>". $row["nazwa"]."</span><span style='border: 1px solid #4FB3DF;width:44.5%;text-align: center;'>".$row["wystepowanie"]."</span></div>" ;
-            }
-        
-    }}
     function Closed(){
         global $conn;
         $conn->close();
