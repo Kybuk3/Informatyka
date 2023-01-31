fa6bf8f8a486e18c40608b34651a730631597af0
Kybuk3 <93988006+Kybuk3@users.noreply.github.com>
2023-01-31 13:12:47 +0100

    Add files via upload

---
+++ exam6/mieszamy.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>malaez</title>
+</head>
+<body> 
+    <div class="baner">
+        <a href="index.php">
+            <h1>Koszt ferby</h1>
+        </a>
+    </div>
+
+    <div class="lewy">
+        <h3>Mieszamy czy wycueniamy?</h3>
+        <a href="mieszamy.php">Mieszamy</a>
+        <a href="cena.php">Wyceniamy</a>
+    </div>
+
+    <div class="prawy">
+        <p>Strona w trakcie budowy</p>
+    </div>
+    <div class="stopka">
+        Malarz<br>
+        Stronę opracował 0000000000
+    </div>
+
+    
+</body>
+</html>
